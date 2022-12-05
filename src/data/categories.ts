7a10bb557e71b496e715f34ea660f68e8881bf7f
Leonardo Brancaglione <43422065+LBrancaglione@users.noreply.github.com>
2022-12-05 12:33:47 -0300

    v1.0

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from "../types/Category"
+
+export const categories: Category = {
+    food: {title: 'alimentation', color: 'blue', expense: true},
+    rent: {title: 'Rent', color:'brown', expense: true},
+    salary: {title: 'Wage', color:'grenn', expense: false}
+}
---
+++ src/data/items.ts
@@ -0,0 +1,9 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+    {date: new Date(2022, 10, 18), category: 'food', title: 'McDonalds', value: 32.12},
+    {date: new Date(2022, 10, 18), category: 'food', title: 'Burger King', value: 28},
+    {date: new Date(2022, 10, 19), category: 'rent', title: 'apartment rent', value: 2300},
+    {date: new Date(2022, 11, 20), category: 'salary', title: 'Work wage', value: 4500},
+    {date: new Date(2022, 11, 20), category: 'salary', title: 'Work', value: 4500},
+];
